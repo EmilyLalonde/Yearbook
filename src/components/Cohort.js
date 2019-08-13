62485b7f86f96b9f5a8c346527cb94d103cdf91d
Emily <lalonde0902@gmail.com>
2019-08-13 14:54:03 -0600

    Add form inputs and button to add new student

--- src/components/Cohort.js
+++ src/components/Cohort.js
@@ -3,14 +3,11 @@ import Person from './Person';
 import './Cohort.css';
 
 const Cohort = (props) => {
-  console.log(props)
-  
 const staff = props.instructors.map((person) => {
           return <Person key={person.id} name={person.name} quote={person.quote} superlative={person.superlative} photo={person.photo}/>
         })
 
  const students = props.students.map((person) => {
-          console.log(person)
           return <Person key={person.id} name={person.name} quote={person.quote} superlative={person.superlative} photo={person.photo}/>
         })
   return (
