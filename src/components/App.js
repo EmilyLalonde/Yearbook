62630fd3d69302b1254630641865bf8f3242adbf
Emily <lalonde0902@gmail.com>
2019-08-14 10:53:50 -0600

    Delete card function works for both staff and students

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Cohort from './Cohort';
 import people from '../data/yearbook-data.js';
 import './App.css';
-import AddStudentForm from '../AddStudentForm';
+import AddStudentForm from './AddStudentForm';
 
 class App extends Component {
   constructor() {
@@ -12,6 +12,7 @@ class App extends Component {
       students: people.students
     }
     this.addStudent = this.addStudent.bind(this);
+    this.deleteCard = this.deleteCard.bind(this);
   }
 
   addStudent(newStudent) {
@@ -20,12 +21,22 @@ class App extends Component {
     })
   }
 
+  deleteCard(id) {
+    const filteredStaff = this.state.staff.filter(instructor => {
+      return instructor.id !== id
+    })
+    const filteredStudents = this.state.students.filter(student => {
+      return student.id !== id
+    })
+    this.setState({staff: filteredStaff, students: filteredStudents});
+  }
+  
   render() {
     return (
       <div className="App">
       <h1>Turing Yearbook</h1>
         <AddStudentForm addStudent={this.addStudent}/>
-        <Cohort instructors={this.state.staff} students={this.state.students}/>
+        <Cohort instructors={this.state.staff} students={this.state.students} deleteCard={this.deleteCard}/>
       </div>
     );
   }
