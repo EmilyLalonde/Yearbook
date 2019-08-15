462e402e5925ffd0d163a38af1cdcb2a454ed825
Emily <lalonde0902@gmail.com>
2019-08-15 14:04:35 -0600

    Add basic setup to start testing

--- src/components/Person.js
+++ src/components/Person.js
@@ -5,9 +5,9 @@ const Person = ({photo, name, quote, superlative, id, deleteCard}) => {
   return (
       <section className="people">
       <img src={photo} alt="yearbook-photos"/>
-      <h3>{name}</h3>
-      <p>{quote}</p>
-      <h4>{superlative}</h4>
+      <h3 contentEditable={true} suppressContentEditableWarning={true}>{name}</h3>
+      <p contentEditable={true} suppressContentEditableWarning={true}>{quote}</p>
+      <h4 contentEditable={true} suppressContentEditableWarning={true}>{superlative}</h4>
       <button type="submit">Update Info</button>
       <button onClick={() => deleteCard(id)}>Delete</button>
       </section>
